04e9abc041df559a21e93c2873fe5303884e5a5c
yhluk <luyinhao@outlook.com>
2022-04-12 00:19:39 +0800

    Update "NotifyDevGroup"

--- src/main/java/lielietea/mirai/plugin/utils/GroupPolice.java
+++ src/main/java/lielietea/mirai/plugin/utils/GroupPolice.java
@@ -32,7 +32,7 @@ public class GroupPolice {
                     if (IdentityUtil.isDevGroup(group.getId())) continue;
                     if (group.getMembers().getSize() < ConfigHandler.getINSTANCE().config.getMinimumMembers()) {
                         group.sendMessage(ConfigHandler.getName(bot)+"目前不接受加入"+ConfigHandler.getINSTANCE().config.getMinimumMembers()+"人以下的群聊，将自动退群，请在其他群中使用"+ConfigHandler.getName(bot)+"。感谢您使用"+ConfigHandler.getName(bot)+"的服务。");
-                        MessageUtil.notifyDevGroup("由于群聊人数不满"+ConfigHandler.getINSTANCE().config.getMinimumMembers()+"人，"+ConfigHandler.getName(bot)+"已经从"+group.getName()+"("+group.getId()+")中离开。");
+                        MessageUtil.notifyDevGroup("由于群聊人数不满"+ConfigHandler.getINSTANCE().config.getMinimumMembers()+"人，"+ConfigHandler.getName(bot)+"已经从"+group.getName()+"("+group.getId()+")中离开。",bot);
                         executor.schedule(() -> {
                             Objects.requireNonNull(bot.getGroup(group.getId())).quit();
                         }, 15, TimeUnit.SECONDS);
--- src/main/java/lielietea/mirai/plugin/utils/MessageUtil.java
+++ src/main/java/lielietea/mirai/plugin/utils/MessageUtil.java
@@ -6,26 +6,9 @@ import net.mamoe.mirai.message.data.MessageChain;
 import net.mamoe.mirai.message.data.MessageChainBuilder;
 
 import java.util.List;
+import java.util.Objects;
 
 public class MessageUtil {
-    /**
-     * 向开发群发送消息通知
-     */
-    public static void notifyDevGroup(String content){
-        List<Bot> bots = Bot.getInstances();
-        for (Bot bot : bots) {
-            Group group = bot.getGroup(IdentityUtil.DevGroup.DEFAULT.getID());
-            if (group != null) group.sendMessage(content);
-        }
-    }
-
-    public static void notifyDevGroup(MessageChain messageChain){
-        List<Bot> bots = Bot.getInstances();
-        for (Bot bot : bots) {
-            Group group = bot.getGroup(IdentityUtil.DevGroup.DEFAULT.getID());
-            if (group != null) group.sendMessage(messageChain);
-        }
-    }
 
     /**
      * 指定某个Bot，向开发群发送消息通知
@@ -34,8 +17,9 @@ public class MessageUtil {
         List<Bot> bots = Bot.getInstances();
         for (Bot bot : bots) {
             if(bot.getId() == botId){
-                Group group = bot.getGroup(IdentityUtil.DevGroup.DEFAULT.getID());
-                if (group != null) group.sendMessage(content);
+                for(long ID:IdentityUtil.getDevGroup()){
+                    if(bot.getGroup(ID)!=null) Objects.requireNonNull(bot.getGroup(ID)).sendMessage(content);
+                }
             }
         }
     }
@@ -44,23 +28,9 @@ public class MessageUtil {
      * 指定某个Bot，向开发群发送消息通知
      */
     public static void notifyDevGroup(String content, Bot bot){
-        Group group = bot.getGroup(IdentityUtil.DevGroup.DEFAULT.getID());
-        if (group != null) group.sendMessage(content);
-    }
-
-    public static void notifyDevGroup(MessageChain messageChain, long botID){
-        List<Bot> bots = Bot.getInstances();
-        for (Bot bot : bots) {
-            if(bot.getId() == botID){
-                Group group = bot.getGroup(IdentityUtil.DevGroup.DEFAULT.getID());
-                if (group != null) group.sendMessage(messageChain);
-            }
+        for(long ID:IdentityUtil.getDevGroup()){
+            if(bot.getGroup(ID)!=null) Objects.requireNonNull(bot.getGroup(ID)).sendMessage(content);
         }
-    }
-
-    public static void notifyDevGroup(MessageChain messageChain, Bot bot){
-        Group group = bot.getGroup(IdentityUtil.DevGroup.DEFAULT.getID());
-        if (group != null) group.sendMessage(messageChain);
     }
 
 }
