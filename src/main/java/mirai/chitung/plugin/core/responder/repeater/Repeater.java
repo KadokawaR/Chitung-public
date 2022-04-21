45cf1208dac0acbaec25c29657ab5161ccd62e0b
yhluk <luyinhao@outlook.com>
2022-04-21 13:01:41 +0800

    Fix errors in Repeater

--- src/main/java/mirai/chitung/plugin/core/responder/repeater/Repeater.java
+++ src/main/java/mirai/chitung/plugin/core/responder/repeater/Repeater.java
@@ -1,40 +1,53 @@
 package mirai.chitung.plugin.core.responder.repeater;
 
 import net.mamoe.mirai.event.events.GroupMessageEvent;
-import net.mamoe.mirai.message.data.MessageChain;
+import net.mamoe.mirai.message.data.*;
 
 import java.util.*;
 
 public class Repeater {
 
-    static Map<Long,List<MessageChain>> messageContainer = new HashMap<>();
+    static Map<Long,List<String>> messageContainer = new HashMap<>();
 
     public static void handle(GroupMessageEvent event){
+
+        if(event.getMessage().serializeToMiraiCode().contains("mirai")) {
+            messageContainer.get(event.getGroup().getId()).clear();
+            return;
+        }
+
+        String message = event.getMessage().contentToString();
+
+        if(message.equals("")){
+            messageContainer.get(event.getGroup().getId()).clear();
+            return;
+        }
+
         if(!messageContainer.containsKey(event.getGroup().getId())){
-            List<MessageChain> temp = new ArrayList<MessageChain>() {{add(event.getMessage());}};
+            List<String> temp = new ArrayList<String>() {{add(message);}};
             messageContainer.put(event.getGroup().getId(),temp);
             return;
         }
 
         if(messageContainer.get(event.getGroup().getId()).size()==0){
-            messageContainer.get(event.getGroup().getId()).add(event.getMessage());
+            messageContainer.get(event.getGroup().getId()).add(message);
             return;
         }
 
         if(messageContainer.get(event.getGroup().getId()).size()==1){
-            if(!messageContainer.get(event.getGroup().getId()).get(0).equals(event.getMessage())){
+            if(!messageContainer.get(event.getGroup().getId()).get(0).equals(message)){
                 messageContainer.get(event.getGroup().getId()).clear();
             }
-            messageContainer.get(event.getGroup().getId()).add(event.getMessage());
+            messageContainer.get(event.getGroup().getId()).add(message);
             return;
         }
 
         if(messageContainer.get(event.getGroup().getId()).size()==2){
-            MessageChain m1 = messageContainer.get(event.getGroup().getId()).get(0);
-            MessageChain m2 = messageContainer.get(event.getGroup().getId()).get(0);
-            MessageChain m3 = event.getMessage();
-            if(m1.equals(m2)&&m2.equals(m3)){
-                event.getGroup().sendMessage(m3);
+            String m1 = messageContainer.get(event.getGroup().getId()).get(0);
+            String m2 = messageContainer.get(event.getGroup().getId()).get(1);
+
+            if(m1.equals(m2)&&m2.equals(message)){
+                event.getGroup().sendMessage(message);
             }
             messageContainer.get(event.getGroup().getId()).clear();
             return;
