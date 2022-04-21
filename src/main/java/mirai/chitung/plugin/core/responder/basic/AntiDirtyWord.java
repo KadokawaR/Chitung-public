28dc6c87760e5a976a69cc22fbcef2a132a5ff78
yhluk <luyinhao@outlook.com>
2022-04-21 11:09:37 +0800

    Revert Anti-dirty-words pattern

--- src/main/java/mirai/chitung/plugin/core/responder/basic/AntiDirtyWord.java
+++ src/main/java/mirai/chitung/plugin/core/responder/basic/AntiDirtyWord.java
@@ -13,11 +13,37 @@ import java.util.regex.Pattern;
 
 public class AntiDirtyWord implements MessageResponder<MessageEvent> {
     static final List<MessageType> TYPES = new ArrayList<>(Arrays.asList(MessageType.FRIEND, MessageType.GROUP));
+
+    /*
     static final Pattern REG_PATTERN = Pattern.compile(".*" + "((([日干操艹草滚槽曹糙])([你尼泥腻妮])([妈马麻码吗玛]))|(([Mm])otherfucker|)|(([Ff])uck ([Yy])ou)|(([Ff])(Uu)(Cc)(Kk)))" + ".*");
 
     @Override
     public boolean match(String content) {
         return REG_PATTERN.matcher(content).matches();
+    }
+
+     */
+
+    static final List<Pattern> REG_PATTERN = new ArrayList<>();
+
+    static {
+        {
+            REG_PATTERN.add(Pattern.compile(".*" + "([日干操艹草滚槽曹糙])([你尼泥腻妮])([妈马麻码吗玛])" + ".*"));
+            REG_PATTERN.add(Pattern.compile(".*" + "([Mm])otherfucker" + ".*"));
+            REG_PATTERN.add(Pattern.compile(".*" + "([Ff])uck ([Yy])ou" + ".*"));
+            REG_PATTERN.add(Pattern.compile(".*" + "([Ff])(Uu)(Cc)(Kk)" + ".*"));
+        }
+    }
+
+    @Override
+    public boolean match(String content) {
+        for (Pattern pattern : REG_PATTERN) {
+            if (pattern.matcher(content).matches()) {
+                return true;
+            }
+        }
+        return false;
+
     }
 
     @Override
