f0c5c3e7713dba565d234fe386b7a73fe6d23555
MarbleGateKeeper <marblegatekeeper@outlook.com>
2022-05-04 13:14:55 +0800

    Fix Write#append

--- src/main/java/mirai/chitung/plugin/utils/fileutils/Write.java
+++ src/main/java/mirai/chitung/plugin/utils/fileutils/Write.java
@@ -4,12 +4,13 @@ import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 
 public class Write {
     //追加写入
     public static void append(String content, String PATH) {
-        try (OutputStreamWriter writer = new OutputStreamWriter(Files.newOutputStream(Paths.get(PATH)), StandardCharsets.UTF_8)) {
-            writer.append(content);
+        try (OutputStreamWriter writer = new OutputStreamWriter(Files.newOutputStream(Paths.get(PATH), StandardOpenOption.APPEND), StandardCharsets.UTF_8)) {
+            writer.write(content);
         } catch (IOException ex) {
             throw new RuntimeException(ex);
         }
